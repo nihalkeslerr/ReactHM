589b74b0475c091163925773c1809bc71ccc2113
nihalselmakesler@gmail.com <88909415+nihalkeslerr@users.noreply.github.com>
2022-11-27 16:16:43 +0300

    Temel olarak fonksiyon yazıldı.

---
+++ index.js
@@ -0,0 +1,20 @@
+import fetch from "node-fetch";
+import axios from "axios";
+
+
+
+async function getData(Number) {
+
+    const {data:users }= await axios("https://jsonplaceholder.typicode.com/users/"+ Number);
+
+    const {data:posts }= await axios("https://jsonplaceholder.typicode.com/posts?id="+ Number);
+
+  //  console.log("user: ",users,posts);
+  //  console.log("Post: ",posts);
+  const array=[users,posts];
+  console.log(array);
+
+}
+
+
+export default getData;
